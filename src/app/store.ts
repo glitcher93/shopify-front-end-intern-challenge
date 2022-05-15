6a16219d258257e654fad751d59830a1e4708e8c
glitcher93 <nigeljdsouza@gmail.com>
2022-05-15 17:45:05 -0400

    created loadState and saveState functions to persist state to localStorage, adjusted configureStore, subscribed the store to saveState function

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,41 @@
 import { configureStore } from "@reduxjs/toolkit";
 import responsesReducer from "../features/responses/responsesSlice";
 
-const reducers = {
-    reducer: {
-        responses: responsesReducer
+const loadState = () => {
+    try {
+        const stringifiedState = localStorage.getItem('state');
+        return stringifiedState ? JSON.parse(stringifiedState) : undefined;
+    } catch (e) {
+        console.log(e);
     }
 }
 
-const store = configureStore(reducers);
+const saveState = (state: RootState) => {
+    try {
+        const stringifiedState = JSON.stringify(state);
+        localStorage.setItem('state', stringifiedState);
+    } catch (e) {
+        console.log(e);
+        return undefined;
+    }
+}
+
+const persistedStore = loadState();
+
+const reducer = {
+    responses: responsesReducer
+}
+
+const store = configureStore({
+    reducer,
+    devTools: true,
+    preloadedState: persistedStore
+});
+
+store.subscribe(() => {
+    const state = store.getState();
+    saveState(state);
+})
 
 export type RootState = ReturnType<typeof store.getState>;
 
