497a4560fde89778453d1ddf32a74fdd38313b8f
glitcher93 <nigeljdsouza@gmail.com>
2022-05-14 20:47:06 -0400

    set up store, and set up RootState and AppDispatch types, exported types and store

--- src/App/store.ts
+++ src/App/store.ts
@@ -1,1 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import responsesReducer from "../features/responses/responsesSlice";
+
+const reducers = {
+    reducer: {
+        responses: responsesReducer
+    }
+}
+
+const store = configureStore(reducers);
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
