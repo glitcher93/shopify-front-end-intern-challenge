6f9e7b64ee3c9a043fba214c9e6659fea7fbffe6
glitcher93 <nigeljdsouza@gmail.com>
2022-05-14 22:27:45 -0400

    imported useDispatch, postPromptForResponse, AppDispatch, ChangeEvent, FormEvent and useState, created handleOnChange and handleSubmit functions

--- src/components/AiForm/AiForm.tsx
+++ src/components/AiForm/AiForm.tsx
@@ -1,9 +1,47 @@
 import './AiForm.scss';
+import { useDispatch } from 'react-redux';
+import { postPromptForResponse } from '../../features/responses/responsesSlice';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { AppDispatch } from '../../app/store';
 
 const AiForm = () => {
+
+    const dispatch = useDispatch<AppDispatch>();
+
+    const [prompt, setPrompt] = useState("");
+    const [required, setRequired] = useState(false)
+
+    const handleOnChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+
+        switch (name) {
+            case "prompt":
+                setPrompt(value);
+                setRequired(false);
+                break;
+            default:
+                return;
+        }
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (!prompt) {
+            setRequired(true);
+            return;
+        }
+
+        dispatch(postPromptForResponse(prompt));
+
+        setPrompt("");
+    }
+
+
     return (
         <form
         className='ai-form'
+        onSubmit={handleSubmit}
         >
             <div
             className='ai-form__group'
@@ -17,7 +55,9 @@ const AiForm = () => {
                 <textarea 
                 name="prompt" 
                 id="prompt"
-                className='ai-form__textarea'
+                className={`ai-form__textarea ${required ? "ai-form__textarea--invalid" : ""}`}
+                value={prompt}
+                onChange={handleOnChange}
                 ></textarea>
             </div>
             <div
