34e6f0289a16c5b5a2c239f71c44c2d6109fd422
glitcher93 <nigeljdsouza@gmail.com>
2022-05-14 20:12:52 -0400

    removed useState import, imported AiForm and Responses components to replace the contents of the form and section components respectively

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,71 +1,24 @@
-import { useState } from 'react';
-import Response from '../features/Response/Response';
+import AiForm from '../components/AiForm/AiForm';
+import Responses from '../features/responses/Responses';
 import './App.scss';
 
 function App() {
-
-  const [articles, setArticle] = useState([{
-    id: "1",
-    prompt: "write a poem",
-    response: "Roses are red, Violets are blue, I don't believe in love, Fuck off"
-  }]);
 
   return (
     <main
     className='main'
     >
-      <h1
-      className='main__title'
+      <div 
+      className="main__wrapper"
       >
-        Fun With AI
-      </h1>
-      <form
-      className='ai-form'
-      >
-        <div
-        className='ai-form__group'
+        <h1
+        className='main__title'
         >
-          <label 
-          htmlFor="prompt"
-          className='ai-form__label'
-          >
-            Enter prompt
-          </label>
-          <textarea 
-          name="prompt" 
-          id="prompt"
-          className='ai-form__textarea'
-          ></textarea>
-        </div>
-        <div
-        className='ai-form__button-container'
-        >
-          <button
-          type='submit'
-          className='ai-form__button'
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-      <section
-      className='responses'
-      >
-        <h2
-        className='responses__title'
-        >
-          Responses
-        </h2>
-        { articles.map(article => {
-          return (
-            <Response
-            key={article.id} 
-            prompt={article.prompt}
-            response={article.response}
-            />
-          )
-        }) }
-      </section>
+          Fun With AI
+        </h1>
+        <AiForm />
+        <Responses />
+      </div>
     </main>
   )
 }
